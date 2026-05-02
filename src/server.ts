d3623a2d596c5fc4f081ddbd774a9061d26c4517
habibchy947 <habibullahalquaderi2005@gmail.com>
2026-05-02 17:37:57 +0600

    server handled gracefully

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,73 @@
+import { Server } from "http";
 import app from "./app";
 import { seedAdmin } from "./app/utils/seed";
 import { envVars } from "./config/env";
 
+let server: Server 
 const bootStrap = async () => {
     try {
         await seedAdmin();
-        app.listen(envVars.PORT, () => {
+        server = app.listen(envVars.PORT, () => {
             console.log(`Server is running on http://localhost:${envVars.PORT}`);
         });
     } catch (error) {
         console.log("Failed to start server", error)
     }
 }
+
+// SIGTERM signal handler
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received. Shutting down server...");
+
+    if(server){
+        server.close(() => {
+            console.log("Server closed gracefully.");
+            process.exit(1);
+        });
+    } 
+    
+    process.exit(1);
+    
+})
+
+// SIGINT signal handler
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal received. Shutting down server...");
+
+    if(server){
+        server.close(() => {
+            console.log("Server closed gracefully.");
+            process.exit(1);
+        });
+
+    }
+
+    process.exit(1);
+});
+
+// uncaught exeption handler
+process.on('uncaughtException', (error) => {
+    console.log("Uncaught Exception Detected... Shutting down server", error);
+
+    if(server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
+})
+
+// unhandled rejection handler
+process.on('unhandledRejection', (error) => {
+    console.log("Unhandled Rejection Detected... Shutting down server", error);
+
+    if(server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
+})
 
 bootStrap();
